6bcd4038773a5c02945561cc1db7fcc6c962ee7b
Abhinav Prime <abhinavprime835@gmail.com>
2024-08-04 16:01:55 +0530

    get url from the html and tests

--- crawl.js
+++ crawl.js
@@ -1,10 +1,38 @@
-function normalizeUrl(urlString)
-{
-    const urlObject=new URL(urlString)
-    const hostPath=`${urlObject.hostname}${urlObject.pathname}` //returning only the hostname and pathname 
-   if(hostPath.length>0 && hostPath.slice(-1)==='/'){
-    return hostPath.slice(0,-1)        //slicing the '/' from the url 
-   }
-   return hostPath
+const { JSDOM } = require("jsdom");
+
+function getUrlsFromHtml(HtmlBody, baseUrl) {
+  const urlsInPage = [];
+  const dom = new JSDOM(HtmlBody); //jsdom converts html code into string
+  const linkElements = dom.window.document.querySelectorAll("a"); //selecting <a> tag from the html body
+  for (const linkelement of linkElements) {
+    if (linkelement.href.slice(0, 1) === "/") {
+      //relative url parsing
+
+      try {
+        const urlObj = new URL(`${baseUrl}${linkelement.href}`);
+        urlsInPage.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with the relative url :${err.message}`);
+      }
+    } else {
+      //absolute url parsing
+      try {
+        const urlObj = new URL(`${linkelement.href}`);
+        urlsInPage.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with the absolute url :${err.message}`);
+      }
+    }
+  }
+  return urlsInPage;
 }
-module.exports={normalizeUrl}
+
+function normalizeUrl(urlString) {
+  const urlObject = new URL(urlString);
+  const hostPath = `${urlObject.hostname}${urlObject.pathname}`; //returning only the hostname and pathname
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1); //slicing the '/' from the url
+  }
+  return hostPath;
+}
+module.exports = { normalizeUrl, getUrlsFromHtml };
--- crawl.test.js
+++ crawl.test.js
@@ -1,31 +1,77 @@
-const {normalizeUrl}=require('./crawl.js')
-const {test,expect}=require('@jest/globals')
+const { normalizeUrl, getUrlsFromHtml } = require("./crawl.js");
+const { test, expect } = require("@jest/globals");
 
-test(normalizeUrl,()=>{
-  const input='https://google.com'
-  const actual=normalizeUrl(input)
-  const expected='google.com'
-  expect(actual).toEqual(expected);  
-  //above lines checks whether the expected and actual outputs are same or not
+test(normalizeUrl, () => {
+  const input = "https://google.com";
+  const actual = normalizeUrl(input);
+  const expected = "google.com";
+  expect(actual).toEqual(expected);
+  //above line checks whether the expected and actual outputs are same or not
+});
+test(normalizeUrl, () => {
+  const input = "http://google.com/";
+  const actual = normalizeUrl(input);
+  const expected = "google.com";
+  expect(actual).toEqual(expected);
+ 
+});
+test(normalizeUrl, () => {
+  const input = "http://GOOGLE.com/";
+  const actual = normalizeUrl(input);
+  const expected = "google.com";
+  expect(actual).toEqual(expected);
+ 
+});
+test(getUrlsFromHtml, () => {
+  const inputHtmlBody = `
+    <html>
+    <body>
+    <a href="http://google.com/">
+    this is google
+    </a>
+    
+    </body>
+    </html>
+    `;
+  const inputBaseUrl = "http://google.com/";
+  const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl);
+  const expected = ["http://google.com/"];
+  expect(actual).toEqual(expected);
+});
 
-}
-)
-test(normalizeUrl,()=>{
-  const input='http://google.com/'
-  const actual=normalizeUrl(input)
-  const expected='google.com'
-  expect(actual).toEqual(expected);  
-  //above lines checks whether the expected and actual outputs are same or not
+//test case 5 for relative and absolute url
+test(getUrlsFromHtml, () => {
+  const inputHtmlBody = `
+    <html>
+    <body>
+    <a href="/path1/">
+    this is google
+    </a>
+    <a href="http://google.com/">
+    this is google
+    </a>
+    </body>
+    </html>
+    `;
+  const inputBaseUrl = "http://google.com";
+  const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl);
+  const expected = ["http://google.com/path1/","http://google.com/"];
+  expect(actual).toEqual(expected);
+});
 
-}
-)
-test(normalizeUrl,()=>{
-  const input='http://GOOGLE.com/'
-  const actual=normalizeUrl(input)
-  const expected='google.com'
-  expect(actual).toEqual(expected);  
-  //above lines checks whether the expected and actual outputs are same or not
-
-}
-)
-
+//test case 6 for checking invalid url
+test(getUrlsFromHtml, () => {
+  const inputHtmlBody = `
+    <html>
+    <body>
+    <a href="invald">
+    this is google
+    </a>
+    </body>
+    </html>
+    `;
+  const inputBaseUrl = "http://google.com";
+  const actual = getUrlsFromHtml(inputHtmlBody, inputBaseUrl);
+  const expected = [];
+  expect(actual).toEqual(expected);
+});
